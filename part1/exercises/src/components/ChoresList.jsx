91c98f750686e484c5694ffa98c5dc6d65b6da4f
Lathansingharath <singharath@hotmail.com>
2024-03-13 21:04:40 -0500

    done

--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,19 @@
+import classes from "./ChoresList.module.css";
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   return (
+      <div>
+         <p className = {classes.choresHeading}>Lance's Chores</p>
+         <ul>
+            <li className={classes.choresText}>Take trash out</li>
+            <li className={classes.choresText}>Washing dishes</li>
+            <li className={classes.choresText}>vacuuming</li>
+
+         
+         </ul>
+
+
+      </div>
+   
+ 
+   );
 }
---
+++ part1/exercises/src/components/HobbyLinks.jsx
@@ -0,0 +1,20 @@
+export default function HobbyLinks(){
+    let hobbyLinks = ["https://www.jpcycles.com","https://archhockey.com/"];
+
+    return (
+        <div>
+            <p>
+               <a href = {hobbyLinks[0]}>jpcycles.com</a>
+            </p>
+            <p>
+               <a href = {hobbyLinks[1]}>archhockey.com</a>
+            </p>
+        </div>
+
+
+
+
+    )
+
+
+}
